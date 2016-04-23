414d102ca2346a369f6c2ae9bf65536a596128a8
yangdongshan <yangdongshan@rd.tuan800.com>
2016-04-23 09:10:00 +0800

    修改运费模板

--- thrift/baseTemplate.thrift
+++ thrift/baseTemplate.thrift
@@ -25,23 +25,40 @@ struct StorehouseQureyParam {
 }
 
 /*********************** 以下是邮费相关结构 ***********************/
+
+/* 邮费模板 */
+struct PostageTemplate {
+    /* 记录ID */
+	1:i32 id,
+	/* 卖家ID */
+	2:i32 sellerId,
+	/* 邮费模板名称 */
+	3:string name,
+	/* 类型 1：按件数  2：按件数上不封顶  3：按重量  4：按重量上不封顶*/
+    4:i32 type
+    /* 邮费信息 */
+    5:list<Postage> postageList
+}
+
 /* 邮费信息 */
 struct Postage {
     /* 记录ID */
 	1:i32 id,
 	/* 卖家ID */
 	2:i32 sellerId,
+	/* 模板ID */
+	3:i32 templateId,
 	/* 邮费名称 */
-	3:string name,
+	4:string name,
 	/* 支持的省份 多个以英文 “,” 隔开*/
-	4:string supportProvince,
+	5:string supportProvince,
     /* 计费规则 */
-    5:string rule,
-    /* 类型 1：按件数  2：按件数上不封顶  3：按重量  4：按重量上不封顶*/
-    6:i32 type
+    6:string rule,
+
 }
-/* 查询邮费参数 */
-struct PostageQureyParam {
+
+/* 查询邮费模板参数 */
+struct PostageTemplateQueryParam {
     /* 记录ID */
     1:i32 id,
     /* 卖家ID */
@@ -51,11 +68,27 @@ struct PostageQureyParam {
     /* 邮费名称 */
     4:optional string name
 }
+/* 查询邮费参数 */
+struct PostageQureyParam {
+    /* 记录ID */
+    1:i32 id,
+    /* 卖家ID */
+    2:i32 sellerId,
+    /* 模板ID */
+    3:i32 templateId
+
+}
 
 /* 仓库接口返回值 */
 struct StorehouseResult {
 	1:result.Result result,
 	2:optional list<Storehouse> storehouseList
+}
+
+/* 邮费接口返回值 */
+struct PostageTemplateResult {
+	1:result.Result result,
+	2:optional list<PostageTemplate> postageTemplateList
 }
 
 /* 邮费接口返回值 */
@@ -81,15 +114,27 @@ service BaseTamplateServ{
 
 
 /*********************** 以下是邮费相关结构 ***********************/
-    /* 添加仓库 */
+
+    /* 添加邮费模板 */
+    result.StringResult addPostageTemplate(1:PostageTemplate postageTemplate);
+    /* 修改邮费模板 */
+    result.Result updatePostageTemplate(1:PostageTemplate postageTemplate);
+    /* 删除邮费模板 */
+    result.Result deletePostageTemplate(1:i32 sellerId, 2:i32 id);
+    /* 删除邮费模板 PostageTemplate实体中需要给sellerId和id赋值，否则删除失败 */
+    result.Result deletePostageTemplagteBatch(1:list<PostageTemplate> postageTemplateList);
+    /* 查询邮费模板 */
+    PostageTemplateResult queryPostageTemplate(1:PostageTemplateQueryParam param);
+
+    /* 添加邮费 */
 	result.StringResult addPostage(1:Postage postage);
-	/* 修改仓库 */
+	/* 修改邮费 */
 	result.Result updatePostage(1:Postage postage);
-	/* 删除仓库 */
+	/* 删除邮费 */
 	result.Result deletePostage(1:i32 sellerId, 2:i32 id);
-	/* 删除仓库 Postage实体中需要给sellerId和id赋值，否则删除失败 */
+	/* 删除邮费 Postage实体中需要给sellerId和id赋值，否则删除失败 */
 	result.Result deletePostageBatch(1:list<Postage> postageList);
-	/* 查询仓库 */
-	StorehouseResult queryPostage(1:PostageQureyParam param);
+	/* 查询邮费 */
+	PostageResult queryPostage(1:PostageQureyParam param);
 	
 }
