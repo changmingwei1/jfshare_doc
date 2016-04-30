b22fe8d21e91156ea969509d74377b7e06da58f4
yangdongshan <yangdongshan@rd.tuan800.com>
2016-04-30 20:02:34 +0800

    添加接口

--- thrift/baseTemplate.thrift
+++ thrift/baseTemplate.thrift
@@ -13,11 +13,11 @@ struct Storehouse {
 	/* 仓库名称 */
 	3:string name,
 	/* 支持的省份 多个以英文 “,” 隔开 */
-	4:string supportProvince,
+	4:string supportProvince
 }
 
 /* 查询仓库参数 */
-struct StorehouseQureyParam {
+struct StorehouseQueryParam {
     /* 记录ID */
     1:i32 id,
     /* 卖家ID */
@@ -25,20 +25,6 @@ struct StorehouseQureyParam {
 }
 
 /*********************** 以下是邮费相关结构 ***********************/
-
-/* 邮费模板 */
-struct PostageTemplate {
-    /* 记录ID */
-	1:i32 id,
-	/* 卖家ID */
-	2:i32 sellerId,
-	/* 邮费模板名称 */
-	3:string name,
-	/* 类型 1：按件数  2：按件数上不封顶  3：按重量  4：按重量上不封顶*/
-    4:i32 type
-    /* 邮费信息 */
-    5:list<Postage> postageList
-}
 
 /* 邮费信息 */
 struct Postage {
@@ -53,8 +39,22 @@ struct Postage {
 	/* 支持的省份 多个以英文 “,” 隔开*/
 	5:string supportProvince,
     /* 计费规则 */
-    6:string rule,
+    6:string rule
 
+}
+
+/* 邮费模板 */
+struct PostageTemplate {
+    /* 记录ID */
+	1:i32 id,
+	/* 卖家ID */
+	2:i32 sellerId,
+	/* 邮费模板名称 */
+	3:string name,
+	/* 类型 1：按件数  2：按件数上不封顶  3：按重量  4：按重量上不封顶*/
+    4:i32 type,
+    /* 邮费信息 */
+    5:list<Postage> postageList
 }
 
 /* 查询邮费模板参数 */
@@ -69,7 +69,7 @@ struct PostageTemplateQueryParam {
     4:optional string name
 }
 /* 查询邮费参数 */
-struct PostageQureyParam {
+struct PostageQueryParam {
     /* 记录ID */
     1:i32 id,
     /* 卖家ID */
@@ -99,7 +99,7 @@ struct PostageResult {
 
 
 
-service BaseTamplateServ{
+service BaseTemplateServ{
 /*********************** 以下是仓库相关接口 ***********************/
     /* 添加仓库 */
 	result.StringResult addStorehouse(1:Storehouse storehouse);
@@ -110,7 +110,9 @@ service BaseTamplateServ{
 	/* 删除仓库 Storehouse实体中需要给sellerId和id赋值，否则删除失败 */
 	result.Result deleteStorehouseBatch(1:list<Storehouse> storehouseList);
 	/* 查询仓库 */
-	StorehouseResult queryStorehouse(1:StorehouseQureyParam param);
+	StorehouseResult queryStorehouse(1:StorehouseQueryParam param);
+	/* 获取仓库信息 */
+	StorehouseResult getStorehouse(1:list<i32> storehouseIds);
 
 
 /*********************** 以下是邮费相关结构 ***********************/
@@ -122,9 +124,12 @@ service BaseTamplateServ{
     /* 删除邮费模板 */
     result.Result deletePostageTemplate(1:i32 sellerId, 2:i32 id);
     /* 删除邮费模板 PostageTemplate实体中需要给sellerId和id赋值，否则删除失败 */
-    result.Result deletePostageTemplagteBatch(1:list<PostageTemplate> postageTemplateList);
+    result.Result deletePostageTemplateBatch(1:list<PostageTemplate> postageTemplateList);
     /* 查询邮费模板 */
     PostageTemplateResult queryPostageTemplate(1:PostageTemplateQueryParam param);
+    /* 获取邮费模板信息 */
+    PostageTemplateResult getPostageTemplate(1:list<i32> postageTemplateIds);
+
 
     /* 添加邮费 */
 	result.StringResult addPostage(1:Postage postage);
@@ -135,6 +140,7 @@ service BaseTamplateServ{
 	/* 删除邮费 Postage实体中需要给sellerId和id赋值，否则删除失败 */
 	result.Result deletePostageBatch(1:list<Postage> postageList);
 	/* 查询邮费 */
-	PostageResult queryPostage(1:PostageQureyParam param);
-	
+	PostageResult queryPostage(1:PostageQueryParam param);
+	/* 获取邮费信息 */
+    PostageResult getPostage(1:list<i32> postageIds);
 }
