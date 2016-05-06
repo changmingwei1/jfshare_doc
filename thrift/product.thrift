9415076c19905b3e713310c5671c5fd19644bb6a
yangdongshan <yangdongshan@rd.tuan800.com>
2016-05-06 09:45:39 +0800

    虚拟商品接口定义

--- thrift/product.thrift
+++ thrift/product.thrift
@@ -103,7 +103,7 @@ struct ProductRetParam {
 	1:i32 baseTag, /*商品基础信息*/	
 	2:i32 skuTemplateTag, /*商品选中sku模板*/
 	3:i32 skuTag,  /*商品sku信息*/	
-	4:i32 attributeTag, /*商品属性信息*/
+	4:i32 attributeTag /*商品属性信息*/
 }
 
 /* 商品概要 */
@@ -141,31 +141,110 @@ struct ProductSurveyQueryBatchParam {
 	2:i32 fromType
 }
 
+struct ProductSkuParam {
+	/* 商品ID */
+	1:string productId,
+	/* 商品sku编码 */
+	2:string skuNum,
+	/* 仓库ID */
+    3:i32 storehouseId,
+}
+
 struct ProductSurveyResult {
       1:result.Result result,
       2:optional pagination.Pagination pagination,
       3:optional list<ProductSurvey> productSurveyList
 }
 
+struct ProductCard {
+	/* 商品ID */
+	1:string productId,
+	/* 卡号 */
+	2:string card_number,
+	/* 密码 */
+	3:string password
+}
+
+struct ProductCardView {
+	/* 商品ID */
+	1:string productId,
+	/* 卡号 */
+	2:string card_number,
+	/* 密码 */
+	3:string password,
+	/* 是否已发放  1：未发放 2：已发放 */
+	4:i32 isUsed
+}
+
+struct ProductCardViewParam {
+	/* 卖家ID */
+	1:i32 sellerId,
+	/* 商品ID */
+	2:string productId,
+	/* 卡号（虚拟商品序列号） */
+	3:string cardNumber,
+	/* 是否已发放  0：全部  1：未发放 2：已发放 */
+	4:i32 isUsed
+}
+
+struct ProductCardStatistics {
+	1:string productId,
+	2:string productName,
+	3:i32 total,
+	4:i32 usedNum,
+	5:i32 unusedNum,
+	6:string createTime
+}
+
+struct ProductCardStatisticsParam {
+	1:i32 sellerId,
+	2:string productName
+}
+
+struct ProductCardParam {
+    /* 商品ID */
+	1:string productId,
+	/* 交易唯一标识 */
+	2:string transactionId,
+	/* 获取卡密个数 */
+	3:i32 num
+}
+
+
+struct ProductCardResult {
+	1:result.Result result,
+	2:list<ProductCard> cardList
+}
+
+struct ProductCardViewListResult {
+	1:result.Result result,
+	2:list<ProductCardView> cardViewList
+}
+
+struct ProductCardStatisticsResult {
+	1:result.Result result,
+	2:list<ProductCardStatistics> cardtatisticsList
+}
+
 struct ProductOpt {
-	
+
 	1:string productId,
 
 	/*修改前的商品状态*/
 	2:i32 curState,
-	
+
 	/*修改后的商品状态*/
 	3:i32 activeState,
-	
+
 	/*原因或内容描述*/
 	4:optional string desc,
-	
+
 	/*操作员id， 卖家-sellerId, 管理员-managerId*/
 	5:string operatorId,
-	
+
 	/*1-卖家， 2-管理员， 3-系统*/
 	6:i32 operatorType,
-	
+
 	/*创建时间*/
 	7:optional string createTime
 }
@@ -181,12 +260,15 @@ service ProductServ {
 	/*查询商品指定sku*/
 	ProductResult queryHotSKU(1:string productId, 2:string skuNum, 3:ProductRetParam param);
 
+	/*查询商品指定sku*/
+	ProductResult queryHotSKUV(1:ProductSkuParam skuParam, 2:ProductRetParam param);
+
 	/*查询商品详情*/
 	result.StringResult queryProductDetail(1:ProductDetailParam param);
-	
+
 	/*创建商品，返回商品ID*/
-	result.StringResult addProduct(1:Product product); 
-	
+	result.StringResult addProduct(1:Product product);
+
 	/*修改商品，返回商品ID*/
 	result.StringResult updateProduct(1:Product product);
 
@@ -198,16 +280,29 @@ service ProductServ {
 
 	/* 管理后台查询商品概要信息列表查询*/
 	ProductSurveyResult productSurveyBackendQuery(1:ProductSurveyQueryParam param);
-	
+
 	/*修改商品状态*/
 	result.Result setProductState(1:ProductOpt productOpt);
-	
+
 	/* 首页楼层商品列表 */
 	ProductSurveyResult queryFloorProduct(1:i32 floorId);
 
 	/*查询商品快照*/
 	ProductResult  queryProductSnap(1:string snapId, 2:ProductRetParam param);
-	
+
 	/*根据商品id批量查询 param.fromType=1从db查询  param.fromType=2 从cache查询*/
 	ProductSurveyResult productSurveyQueryBatch(1:ProductSurveyQueryBatchParam param);
+
+	/* 获取虚拟商品卡密（关键接口） */
+	ProductCardResult getProductCard(ProductCardParam param);
+
+	/* 查询虚拟商品卡密，根据transactionId，查询卡密信息 */
+	ProductCardResult queryProductCard(ProductCardParam param);
+
+	/* 查询虚拟商品卡密统计列表，管理专用 */
+	ProductCardStatisticsResult statisticsProductCard(ProductCardStatisticsParam param);
+
+	/* 查询单个虚拟商品卡密列表，管理专用 */
+	ProductCardViewListResult queryProductCardViewList(ProductCardViewParam param);
+
 }
