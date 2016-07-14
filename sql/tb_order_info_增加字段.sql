8024b2bff837b4d57c471455d0aa319bf72a1156
changmingwei <changmingwei@feinno.com>
2016-07-14 13:36:43 +0800

    shrift文件doc

--- sql/tb_order_info_增加字段.sql
+++
@@ -1,8 +0,0 @@
-﻿SELECT CONCAT("ALTER TABLE ",table_name," ADD `postage` int(11) DEFAULT '0' COMMENT '邮费',ADD `postage_template_id` int(11) DEFAULT '0' COMMENT '运费模板id',ADD `storehouse_id` int(11) DEFAULT '0' COMMENT '仓库id', ADD `thirdExchangeRate` varchar(16) DEFAULT NULL COMMENT '第三方积分抵现比率 如:10，1分抵10分钱';") AS create_sql
-FROM information_schema.TABLES WHERE table_name like 'tb_order_info_%'
-
-
-
-
-
-11111122223333
