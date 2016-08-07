ae779953f815e3c4a65dceea53bb1ae4d3c5707b
changmingwei <changmingwei@feinno.com>
2016-08-07 18:04:32 +0800

    解决冲突buyer。thrift1804

--- thrift/buyer.thrift
+++ thrift/buyer.thrift
@@ -46,7 +46,9 @@ struct Buyer {
     22:string remark,
     23:i32 serial,
     24:string createTime,
-    25:string lastUpdateTime
+    25:string lastUpdateTime,
+	26:i32 state,
+	27:i32 clientType
 }
 
 /*登陆请求参数*/
