7dd6e1fba7b264f632417bba8387d7bed53c1f2a
changmingwei <changmingwei@feinno.com>
2016-11-14 11:55:17 +0800

    第三方支付

--- thrift/thirdPay.thrift
+++ thrift/thirdPay.thrift
@@ -1,15 +1,15 @@
-namespace java com.jfshare.finagle.thrift.pay
+namespace java com.jfshare.finagle.thrift.thirdpay
 
 include "result.thrift"
 
 
-stuct ThirdPayApplyResult{
+struct ThirdPayApplyResult{
 	1:string action,		/*登陆页面URL*/
 	2:string encryptyParam, /*加密参数*/
 }
 /*第三方支付服务*/
-service PayServ {
+service ThirdPayServ {
      /*第三方申请支付请求*/
-	result.ThirdPayApplyResult thirdPayApply(1:string requestXml);
+	ThirdPayApplyResult thirdPayApply(1:string requestXml);
 
 }
